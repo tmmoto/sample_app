32b15476f8e94a2288c92b71cfbe7f4c3044fc0a
Tibor <tibor.mozes+dev@gmail.com>
2010-10-21 18:19:09 -0400

    Added 7 test cases

--- config/routes.rb
+++ config/routes.rb
@@ -5,6 +5,8 @@ SampleApp::Application.routes.draw do
 
   get "pages/contact"
   
+  get "pages/about"
+    
   root :to => "Pages#home"
   # The priority is based upon order of creation:
   # first created -> highest priority.
