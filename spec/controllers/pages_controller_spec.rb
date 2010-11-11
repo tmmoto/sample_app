6904a171fd09a1cf86087602ee475209e6bd4e85
Tibor <tibor.mozes+dev@gmail.com>
2010-11-11 18:10:26 -0500

    Added some user model validations

--- spec/controllers/pages_controller_spec.rb
+++ spec/controllers/pages_controller_spec.rb
@@ -12,7 +12,7 @@ describe PagesController do
       get 'home'
       response.should be_success
     end 
-    
+     
     it  "should have the right title" do
       get 'home'
       response.should have_selector("title",
