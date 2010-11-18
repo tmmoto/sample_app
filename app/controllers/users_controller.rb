6ecb8efd227a809772a7d886419bde6ae8885415
Tibor <tibor.mozes+dev@gmail.com>
2010-11-18 18:00:49 -0500

    Added a signup form

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -7,6 +7,8 @@ class UsersController < ApplicationController
 
   def new
     @title = "Sign Up"
+    @user = User.new    # Creating a raw user object here, not yet save in the database.
+    
   end
 
 end
