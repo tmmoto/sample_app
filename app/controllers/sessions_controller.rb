be4cc67dbf0b41e7af7468c80343b5cd88927995
Tibor <tibor.mozes+dev@gmail.com>
2010-11-22 18:55:42 -0500

    Handling invalid signins

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -2,10 +2,20 @@ class SessionsController < ApplicationController
   
   def new
     @title ="Sign in"
+    
   end
   
   def create
-    
+    user = User.authenticate( params[:session][:email], 
+                              params[:session][:password] )
+                              
+    if user.nil? 
+      flash.now[:error] = "Invalid email/password combination."  #Without flash.now, flash will be descplayed in the render new (part of current request), and the next request a
+      @title ="Sign in"
+      render 'new'    # In case of a failed create, we want to rerender the 'new' page
+    else
+      # Handle successful signin
+    end    
   end
   
   def destroy
