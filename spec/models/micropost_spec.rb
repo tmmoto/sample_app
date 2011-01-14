35318fddea3237f66a5a31fdefa91f760228591c
Tibor <tibor.mozes+dev@gmail.com>
2011-01-13 19:42:52 -0500

    Finished Micropost validations

--- spec/models/micropost_spec.rb
+++ spec/models/micropost_spec.rb
@@ -4,7 +4,7 @@ describe Micropost do
   
   before(:each) do
     @user = Factory(:user)
-    @attr = { :contet => "foo bar" }
+    @attr = { :content => "foo bar" }
   end
   
   it "should create a new instance with valid attributes" do
@@ -28,9 +28,21 @@ describe Micropost do
       @micropost.user_id.should == @user.id
       @micropost.user.should == @user
     end
-  
   end
   
-  
+  describe "validations" do
+    
+    it "should have a user id" do
+      Micropost.new(@attr).should_not be_valid
+    end
+    
+    it "should require nonblank content" do
+      @user.microposts.build(:content=> "   ").should_not be_valid
+    end
+    
+    it "should reject long content" do
+      @user.microposts.build(:content => "a" * 141).should_not be_valid
+    end
+  end
 
 end
